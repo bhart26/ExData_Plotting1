851589a6305dafa495a1642161a009f121d2d1bc
bhart26 <bhartigajera@yahoo.com>
2015-04-12 19:33:20 -0400

    Update plot3.r

--- plot3.r
+++ plot3.r
@@ -4,4 +4,10 @@ datetime <- strptime(paste(subsetData$Date, subsetData$Time, sep=" "), "%d/%m/%Y
 subsetMetering1 <- as.numeric(subsetData$Sub_metering_1)
 subsetMetering2 <- as.numeric(subsetData$Sub_metering_2)
 subsetMetering3 <- as.numeric(subsetData$Sub_metering_3)
+png("plot3.png", width=480, height=480)
+plot(datetime,subsetMetering1, type="l",ylab="Energy Submetering", xlab="")
+lines(datetime, subsetMetering2, type="l", col="red")
+lines(datetime, subsetMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black", "red", "blue"))
+dev.off()
 
